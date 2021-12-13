86513d7535c2503135fcedacd087ea5ff2c3eeab
Juan Carlos Diaz Peña <juan.diaz4mt@correo.usa.edu.co>
2021-12-13 00:46:34 -0500

    first commit

---
+++ src/main/java/Reto2_Web/servicio/OrderService.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reto2_Web.servicio;
+
+import Reto2_Web.modelo.Order;
+import Reto2_Web.repositorio.OrderRepositorio;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Usuario
+ */
+
+@Service
+public class OrderService {
+       @Autowired
+    private OrderRepositorio orderRepository;
+
+    public List<Order> getAll(){
+        return orderRepository.getAll();
+    }
+
+    public Optional<Order> getOrder(Integer id){
+        return orderRepository.getOrder(id);
+    }
+
+    public Order create(Order order){
+        if (order.getId() == null){
+            return order;
+        } else {
+            return orderRepository.create(order);
+        }
+    }
+
+    public Order update(Order order){
+        if (order.getId() != null){
+            Optional<Order> dbOrder = orderRepository.getOrder(order.getId());
+            if (!dbOrder.isEmpty()) {
+
+                if (order.getId() != null) {
+                    dbOrder.get().setId(order.getId());
+                }
+
+                if (order.getRegisterDay() != null) {
+                    dbOrder.get().setRegisterDay(order.getRegisterDay());
+                }
+
+                if (order.getStatus() != null) {
+                    dbOrder.get().setStatus(order.getStatus());
+                }
+
+                if (order.getSalesMan() != null) {
+                    dbOrder.get().setSalesMan(order.getSalesMan());
+                }
+
+                if (order.getProducts() != null) {
+                    dbOrder.get().setProducts(order.getProducts());
+                }
+
+                if (order.getQuantities() != null) {
+                    dbOrder.get().setQuantities(order.getQuantities());
+                }
+                orderRepository.update(dbOrder.get());
+                return dbOrder.get();
+            } else {
+                return order;
+            }
+        } else {
+            return order;
+        }
+    }
+
+    public boolean delete(Integer id){
+        return getOrder(id).map(order -> {
+            orderRepository.delete(order);
+            return true;
+        }).orElse(false);
+    }
+
+    public List<Order> getOrderByZone(String zone){
+        return orderRepository.getOrderByZone(zone);
+    }
+    
+}
